70857fe903a3b10476739c1cba4b8d208bcfbe4e
Maël <contact@maeln.com>
2019-02-26 17:40:14 +0100

    FPS cam.

---
+++ src/camera/mod.rs
@@ -0,0 +1,67 @@
+use cgmath::prelude::*;
+use cgmath::{perspective, Deg, Matrix4, Point3, Vector3};
+
+pub enum Direction {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+}
+
+#[derive(Debug)]
+pub struct Camera {
+    pub position: Point3<f32>,
+    pub forward: Vector3<f32>,
+    pub world_up: Vector3<f32>,
+    pub right: Vector3<f32>,
+    pub up: Vector3<f32>,
+
+    angle_h: f32,
+    angle_v: f32,
+}
+
+impl Camera {
+    pub fn new(position: Point3<f32>, forward: Vector3<f32>, world_up: Vector3<f32>) -> Camera {
+        let right = forward.cross(world_up).normalize();
+        let up = right.cross(forward).normalize();
+
+        Camera {
+            position,
+            forward,
+            world_up,
+            right,
+            up,
+            angle_h: 0.0,
+            angle_v: 0.0,
+        }
+    }
+
+    pub fn view(&self) -> Matrix4<f32> {
+        Matrix4::look_at(self.position, self.position + self.forward, self.up)
+    }
+
+    pub fn move_cam(&mut self, dir: &Direction, dt: f32) {
+        let vel = 5.0 * dt;
+        match dir {
+            &Direction::FORWARD => self.position = self.position + self.forward * vel,
+            &Direction::BACKWARD => self.position = self.position - self.forward * vel,
+            &Direction::LEFT => self.position = self.position - self.right * vel,
+            &Direction::RIGHT => self.position = self.position + self.right * vel,
+        }
+    }
+
+    pub fn move_target(&mut self, dx: f32, dy: f32, dt: f32) {
+        let vel = 1.0 * dt;
+        self.angle_h = self.angle_h + dx * vel;
+        self.angle_v = self.angle_v + dy * vel;
+
+        self.forward = Vector3::new(
+            f32::cos(self.angle_v) * f32::sin(self.angle_h),
+            f32::sin(self.angle_v),
+            f32::cos(self.angle_v) * f32::cos(self.angle_h),
+        );
+
+        self.right = self.forward.cross(self.world_up).normalize();
+        self.up = self.right.cross(self.forward).normalize();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate cgmath;
 extern crate gl;
 extern crate glutin;
 
+mod camera;
 mod mesh;
 mod shaders;
 mod utils;
@@ -9,7 +10,9 @@ mod utils;
 use glutin::GlContext;
 use std::path::Path;
 use std::rc::Rc;
+use std::time::{Duration, Instant};
 
+use camera::{Camera, Direction};
 use shaders::{Program, Shader};
 
 use cgmath::prelude::*;
@@ -29,36 +32,35 @@ fn main() {
     }
 
     let vertex_shader = Shader::load_shader(Path::new("data/shaders/basic/projection.vs"));
-    println!("{:?}", vertex_shader);
-
     let pixel_shader = Shader::load_shader(Path::new("data/shaders/basic/phong/phong.fs"));
-    println!("{:?}", pixel_shader);
 
     let program = Program::load_program(&vec![
         Rc::new(pixel_shader.unwrap()),
         Rc::new(vertex_shader.unwrap()),
     ])
     .unwrap();
-    println!("{:?}", program);
 
     let mut cube = mesh::Mesh::cube();
     cube.ready_up();
-    println!("{:?}", cube);
-    cube.draw();
-
-    let projection = perspective(Deg(75.0), 1024.0 / 768.0, 0.1, 10.0);
-
-    let cam_pos = Point3::new(3.0, 2.0, 3.0);
-    let cam_target = Point3::new(0.0, 0.0, 0.0);
-    let cam_forward = cam_target - cam_pos;
-    let cam_right = cam_forward.cross(Vector3::new(0.0, 1.0, 0.0)).normalize();
-    let cam_up = cam_right.cross(cam_forward).normalize();
-    let view = Matrix4::look_at(cam_pos, cam_target, cam_up);
 
     let mut model = Matrix4::<f32>::identity();
+    let projection = perspective(Deg(75.0), 1024.0 / 768.0, 0.1, 10.0);
+    let mut cam = Camera::new(
+        Point3::new(0.0, 0.0, -2.0),
+        Vector3::new(0.0, 0.0, 1.0),
+        Vector3::new(0.0, 1.0, 0.0),
+    );
+
+    let mut time = Instant::now();
+    let mut dt = 0.0;
+
+    let mut mouse_pos: (f32, f32) = (0.0, 0.0);
+    let mut mouse_delta: (f32, f32) = (0.0, 0.0);
+    let mut mouse_pressed = false;
 
     let mut running = true;
     while running {
+        mouse_delta = (0.0, 0.0);
         events_loop.poll_events(|event| match event {
             glutin::Event::WindowEvent { event, .. } => match event {
                 glutin::WindowEvent::CloseRequested => running = false,
@@ -69,12 +71,59 @@ fn main() {
                         {
                             running = false
                         }
+
+                        if vkey == glutin::VirtualKeyCode::W
+                            && input.state == glutin::ElementState::Pressed
+                        {
+                            cam.move_cam(&Direction::FORWARD, dt);
+                        }
+
+                        if vkey == glutin::VirtualKeyCode::S
+                            && input.state == glutin::ElementState::Pressed
+                        {
+                            cam.move_cam(&Direction::BACKWARD, dt);
+                        }
+
+                        if vkey == glutin::VirtualKeyCode::A
+                            && input.state == glutin::ElementState::Pressed
+                        {
+                            cam.move_cam(&Direction::LEFT, dt);
+                        }
+
+                        if vkey == glutin::VirtualKeyCode::D
+                            && input.state == glutin::ElementState::Pressed
+                        {
+                            cam.move_cam(&Direction::RIGHT, dt);
+                        }
+                    }
+                }
+                glutin::WindowEvent::CursorMoved { position, .. } => {
+                    mouse_delta = (
+                        mouse_pos.0 - position.x as f32,
+                        mouse_pos.1 - position.y as f32,
+                    );
+                    mouse_pos = (position.x as f32, position.y as f32);
+                }
+                glutin::WindowEvent::MouseInput { state, button, .. } => {
+                    if state == glutin::ElementState::Pressed && button == glutin::MouseButton::Left
+                    {
+                        mouse_pressed = true;
+                    }
+
+                    if state == glutin::ElementState::Released
+                        && button == glutin::MouseButton::Left
+                    {
+                        mouse_pressed = false;
                     }
                 }
                 _ => (),
             },
             _ => (),
         });
+
+        if mouse_pressed {
+            cam.move_target(mouse_delta.0, mouse_delta.1, dt);
+        }
 
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT | gl::STENCIL_BUFFER_BIT);
@@ -83,13 +132,18 @@ fn main() {
 
         program.bind();
         program.set_mat4("projection", &projection);
-        program.set_mat4("view", &view);
+        program.set_mat4("view", &cam.view());
         program.set_mat4("model", &model);
-        program.set_vec4("eye_pos", &cam_pos.to_homogeneous());
+        program.set_vec4("eye_pos", &cam.position.to_homogeneous());
         program.set_vec4("light_pos", &Point3::new(3.0, 1.0, 1.0).to_homogeneous());
 
         cube.draw();
 
         gl_window.swap_buffers().unwrap();
+
+        let end = Instant::now();
+        let delta = end - time;
+        dt = (delta.subsec_millis() as f32) / 1000.0;
+        time = end;
     }
 }
