23a79907f5157d0abb7766b7cc307c2d5336fe5e
Maël <contact@maeln.com>
2019-02-26 14:02:54 +0100

    Add a cube exemple.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate cgmath;
 extern crate gl;
 extern crate glutin;
 
@@ -8,6 +9,11 @@ mod utils;
 use glutin::GlContext;
 use std::path::Path;
 use std::rc::Rc;
+
+use shaders::{Program, Shader};
+
+use cgmath::prelude::*;
+use cgmath::{perspective, Deg, Matrix4, Point3, Vector3};
 
 fn main() {
     let mut events_loop = glutin::EventsLoop::new();
@@ -22,16 +28,34 @@ fn main() {
         gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
     }
 
-    let shader = shaders::shader_loader::load_shader(Path::new("data/shaders/plane/plane.vs"));
-    println!("{:?}", shader);
+    let vertex_shader = Shader::load_shader(Path::new("data/shaders/basic/projection.vs"));
+    println!("{:?}", vertex_shader);
 
-    let program = shaders::shader_loader::load_program(&vec![Rc::new(shader.unwrap())]);
+    let pixel_shader = Shader::load_shader(Path::new("data/shaders/basic/phong/phong.fs"));
+    println!("{:?}", pixel_shader);
+
+    let program = Program::load_program(&vec![
+        Rc::new(pixel_shader.unwrap()),
+        Rc::new(vertex_shader.unwrap()),
+    ])
+    .unwrap();
     println!("{:?}", program);
 
     let mut cube = mesh::Mesh::cube();
     cube.ready_up();
     println!("{:?}", cube);
     cube.draw();
+
+    let projection = perspective(Deg(75.0), 1024.0 / 768.0, 0.1, 10.0);
+
+    let cam_pos = Point3::new(3.0, 2.0, 3.0);
+    let cam_target = Point3::new(0.0, 0.0, 0.0);
+    let cam_forward = cam_target - cam_pos;
+    let cam_right = cam_forward.cross(Vector3::new(0.0, 1.0, 0.0)).normalize();
+    let cam_up = cam_right.cross(cam_forward).normalize();
+    let view = Matrix4::look_at(cam_pos, cam_target, cam_up);
+
+    let mut model = Matrix4::<f32>::identity();
 
     let mut running = true;
     while running {
@@ -56,6 +80,15 @@ fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT | gl::STENCIL_BUFFER_BIT);
             gl::ClearColor(0.0, 0.0, 0.0, 0.0);
         }
+
+        program.bind();
+        program.set_mat4("projection", &projection);
+        program.set_mat4("view", &view);
+        program.set_mat4("model", &model);
+        program.set_vec4("eye_pos", &cam_pos.to_homogeneous());
+        program.set_vec4("light_pos", &Point3::new(3.0, 1.0, 1.0).to_homogeneous());
+
+        cube.draw();
 
         gl_window.swap_buffers().unwrap();
     }
--- src/shaders/shader_loader.rs
+++ src/shaders/shader_loader.rs
@@ -6,6 +6,8 @@ use std::ffi::CString;
 use std::ptr;
 use std::rc::Rc;
 
+use cgmath::{Array, Matrix, Matrix4, Vector2, Vector3, Vector4};
+
 use super::*;
 
 pub fn parse_uniforms(src: &str) -> Vec<String> {
@@ -25,111 +27,162 @@ pub fn parse_uniforms(src: &str) -> Vec<String> {
     uniforms
 }
 
-pub fn load_shader(path: &Path) -> Option<Shader> {
-    #[cfg(feature = "debug")]
-    println!("[NFO] Loading shader {}", path.display());
-
-    let src = utils::load_file(path).and_then(|shd_src| CString::new(shd_src.as_bytes()).ok());
-    if src.is_none() {
+impl Shader {
+    pub fn load_shader(path: &Path) -> Option<Shader> {
         #[cfg(feature = "debug")]
-        eprintln!("[ERR] Couldn't load source for shader {}", path.display());
+        println!("[NFO] Loading shader {}", path.display());
 
-        return None;
-    }
-    let src = src.unwrap();
-
-    let shader_type = get_shader_type(path);
-    if shader_type.is_none() {
-        #[cfg(feature = "debug")]
-        eprintln!("[ERR] Couldn't detect shader type for {}", path.display());
-
-        return None;
-    }
-
-    let gl_type = get_gl_shader_type(&shader_type);
-    unsafe {
-        let addr = gl::CreateShader(gl_type.unwrap());
-        gl::ShaderSource(addr, 1, &src.as_ptr(), ptr::null());
-        gl::CompileShader(addr);
-
-        let mut status: i32 = 0;
-        gl::GetShaderiv(addr, gl::COMPILE_STATUS, &mut status);
-        if status == i32::from(gl::FALSE) {
+        let src = utils::load_file(path).and_then(|shd_src| CString::new(shd_src.as_bytes()).ok());
+        if src.is_none() {
             #[cfg(feature = "debug")]
-            {
-                let mut log_len: i32 = 0;
-                gl::GetShaderiv(addr, gl::INFO_LOG_LENGTH, &mut log_len);
-                let mut log: Vec<u8> = Vec::with_capacity(log_len as usize);
-                gl::GetShaderInfoLog(addr, log_len, ptr::null_mut(), log.as_mut_ptr() as *mut i8);
-                log.set_len(log_len as usize);
-                eprintln!(
-                    "[ERR] Couldn't compile shader {}, log:\n{}",
-                    path.display(),
-                    String::from_utf8_lossy(&log[..])
-                );
-            }
+            eprintln!("[ERR] Couldn't load source for shader {}", path.display());
+
+            return None;
+        }
+        let src = src.unwrap();
+
+        let shader_type = get_shader_type(path);
+        if shader_type.is_none() {
+            #[cfg(feature = "debug")]
+            eprintln!("[ERR] Couldn't detect shader type for {}", path.display());
 
             return None;
         }
 
-        Some(Shader {
-            addr,
-            path: String::from(path.to_str().unwrap()),
-            uniforms: parse_uniforms(&src.to_string_lossy()),
-            shader_type: shader_type.unwrap(),
-        })
+        let gl_type = get_gl_shader_type(&shader_type);
+        unsafe {
+            let addr = gl::CreateShader(gl_type.unwrap());
+            gl::ShaderSource(addr, 1, &src.as_ptr(), ptr::null());
+            gl::CompileShader(addr);
+
+            let mut status: i32 = 0;
+            gl::GetShaderiv(addr, gl::COMPILE_STATUS, &mut status);
+            if status == i32::from(gl::FALSE) {
+                #[cfg(feature = "debug")]
+                {
+                    let mut log_len: i32 = 0;
+                    gl::GetShaderiv(addr, gl::INFO_LOG_LENGTH, &mut log_len);
+                    let mut log: Vec<u8> = Vec::with_capacity(log_len as usize);
+                    gl::GetShaderInfoLog(
+                        addr,
+                        log_len,
+                        ptr::null_mut(),
+                        log.as_mut_ptr() as *mut i8,
+                    );
+                    log.set_len(log_len as usize);
+                    eprintln!(
+                        "[ERR] Couldn't compile shader {}, log:\n{}",
+                        path.display(),
+                        String::from_utf8_lossy(&log[..])
+                    );
+                }
+
+                return None;
+            }
+
+            Some(Shader {
+                addr,
+                path: String::from(path.to_str().unwrap()),
+                uniforms: parse_uniforms(&src.to_string_lossy()),
+                shader_type: shader_type.unwrap(),
+            })
+        }
     }
 }
 
-pub fn load_program(shaders: &Vec<Rc<Shader>>) -> Option<Program> {
-    unsafe {
-        let addr = gl::CreateProgram();
-        for shader in shaders {
-            gl::AttachShader(addr, shader.addr);
+impl Program {
+    pub fn bind(&self) {
+        unsafe {
+            gl::UseProgram(self.addr);
         }
-        gl::LinkProgram(addr);
+    }
 
-        let mut status: i32 = 0;
-        gl::GetProgramiv(addr, gl::LINK_STATUS, &mut status);
-        if status == i32::from(gl::FALSE) {
-            #[cfg(feature = "debug")]
-            {
-                let mut log_length: i32 = 0;
-                gl::GetProgramiv(addr, gl::INFO_LOG_LENGTH, &mut log_length);
-                let mut log: Vec<u8> = Vec::with_capacity(log_length as usize);
-                gl::GetProgramInfoLog(
-                    addr,
-                    log_length,
-                    ptr::null_mut(),
-                    log.as_mut_ptr() as *mut i8,
-                );
-                log.set_len(log_length as usize);
-                eprintln!(
-                    "[ERR] Couldn't link program, log:\n{}",
-                    String::from_utf8_lossy(&log[..])
-                );
+    pub fn unbind() {
+        unsafe {
+            gl::UseProgram(0);
+        }
+    }
+
+    pub fn set_float(&self, name: &str, value: f32) {
+        unsafe {
+            gl::Uniform1f(self.uniforms_location[name], value);
+        }
+    }
+
+    pub fn set_vec2(&self, name: &str, value: &Vector2<f32>) {
+        unsafe {
+            gl::Uniform2fv(self.uniforms_location[name], 1, value.as_ptr());
+        }
+    }
+
+    pub fn set_vec3(&self, name: &str, value: &Vector3<f32>) {
+        unsafe {
+            gl::Uniform3fv(self.uniforms_location[name], 1, value.as_ptr());
+        }
+    }
+
+    pub fn set_vec4(&self, name: &str, value: &Vector4<f32>) {
+        unsafe {
+            gl::Uniform4fv(self.uniforms_location[name], 1, value.as_ptr());
+        }
+    }
+
+    pub fn set_mat4(&self, name: &str, value: &Matrix4<f32>) {
+        unsafe {
+            gl::UniformMatrix4fv(self.uniforms_location[name], 1, gl::FALSE, value.as_ptr());
+        }
+    }
+
+    pub fn load_program(shaders: &Vec<Rc<Shader>>) -> Option<Program> {
+        unsafe {
+            let addr = gl::CreateProgram();
+            for shader in shaders {
+                gl::AttachShader(addr, shader.addr);
+            }
+            gl::LinkProgram(addr);
+
+            let mut status: i32 = 0;
+            gl::GetProgramiv(addr, gl::LINK_STATUS, &mut status);
+            if status == i32::from(gl::FALSE) {
+                #[cfg(feature = "debug")]
+                {
+                    let mut log_length: i32 = 0;
+                    gl::GetProgramiv(addr, gl::INFO_LOG_LENGTH, &mut log_length);
+                    let mut log: Vec<u8> = Vec::with_capacity(log_length as usize);
+                    gl::GetProgramInfoLog(
+                        addr,
+                        log_length,
+                        ptr::null_mut(),
+                        log.as_mut_ptr() as *mut i8,
+                    );
+                    log.set_len(log_length as usize);
+                    eprintln!(
+                        "[ERR] Couldn't link program, log:\n{}",
+                        String::from_utf8_lossy(&log[..])
+                    );
+                }
+
+                return None;
             }
 
-            return None;
-        }
+            let mut program = Program {
+                addr,
+                shaders: Vec::with_capacity(shaders.len()),
+                uniforms_location: HashMap::new(),
+            };
 
-        let mut program = Program {
-            addr,
-            shaders: Vec::with_capacity(shaders.len()),
-            uniforms_location: HashMap::new(),
-        };
+            for shader in shaders.into_iter() {
+                gl::DetachShader(addr, shader.addr);
+                program.shaders.push(Rc::clone(shader));
 
-        for shader in shaders.into_iter() {
-            gl::DetachShader(addr, shader.addr);
-            program.shaders.push(Rc::clone(shader));
-
-            for uniform in &shader.uniforms {
-                let uniform_cstr = CString::new(uniform.as_bytes()).unwrap();
-                let location = gl::GetUniformLocation(addr, uniform_cstr.as_ptr());
-                program.uniforms_location.insert(uniform.clone(), location);
+                for uniform in &shader.uniforms {
+                    let uniform_cstr = CString::new(uniform.as_bytes()).unwrap();
+                    let location = gl::GetUniformLocation(addr, uniform_cstr.as_ptr());
+                    program.uniforms_location.insert(uniform.clone(), location);
+                }
             }
-        }
 
-        Some(program)
+            Some(program)
+        }
     }
 }
